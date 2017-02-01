557f8e1fe6c5b58b3f9e01a75f025560a21c4c9f
Hack Reactor Students <accounts@hackreactor.com>
2017-02-01 15:14:28 -0600

    all test for sprint one passing

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  storage.length = 0;
+
+  _.extend(storage, stackMethods);
+
+  return storage;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this[this.length] = value;
+    this.length++;
+  },
+  pop: function() {
+    if (this.length > 0) {
+      this.length--;
+      var deletedItem = this[this.length];
+      delete this[this.length];
+
+      return deletedItem;
+    }
+  },
+  size: function() {
+    return this.length;
+  }
+};
 
 
--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var storage = Object.create(queueMethods);
+  storage.length = 0;
+
+  return storage;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this[this.length] = value;
+    this.length++;
+  },
+  dequeue: function(){
+    if(this.length > 0){
+      var deletedItem = this[0];
+      delete this[0];
+
+      for(var key in this){
+        this[key-1] = this[key];
+
+      }
+      delete this[this.length];
+      this.length--;
+      return deletedItem;
+    }
+  },
+  size: function(){
+    return this.length;
+  }
+};
 
 
