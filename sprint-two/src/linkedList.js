402650fe1e571508e7f6d51cb28087fda5547ce1
Hack Reactor Students <accounts@hackreactor.com>
2017-02-01 16:59:35 -0600

    Implemented linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,45 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    //handle if linkedlist is empty
+    if(list.head === null){
+      list.head = node;
+      list.tail = node;
+    }else{
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    //handle if one item
+    var deletedItem = list.head;
+
+    if (list.head === list.tail) {
+      list.head = null;
+      list.tail = null;
+    } else if (deletedItem) {
+      list.head = list.head.next;
+    }
+
+    return deletedItem.value;
   };
 
   list.contains = function(target) {
+    var result = false;
+    var search = function(node){
+      if(node.value === target){
+        result =  true;
+      }else{
+        if(node.next){
+          search(node.next);          
+        }
+      }
+    };
+
+    if (list.head) search(list.head);
+    return result;
   };
 
   return list;
