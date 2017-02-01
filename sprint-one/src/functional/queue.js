6b2f0c75366ba95a1bc46282962126499b1fb13e
Hack Reactor Students <accounts@hackreactor.com>
2017-02-01 11:42:43 -0600

    got functional queue passing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,31 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    if(size > 0){
+      var deletedItem = storage[0];
+      delete storage[0];
+      for(var key in storage){
+        storage[key-1] = storage[key];
+      }
+      delete storage[size];
+      size--;
+
+      return deletedItem;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
