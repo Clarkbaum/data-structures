dfaba4f4c1596c9fe1323cedd300d8957be2b1b2
Hack Reactor Students <accounts@hackreactor.com>
2017-02-02 15:30:25 -0600

    Added tests to setSpec.js

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -18,10 +18,23 @@ describe('set', function() {
     expect(set.contains('Susan Sarandon')).to.equal(true);
   });
 
+  it('should increment the value when adding more items in set', function(){
+    set.add('Joe');
+    set.add('Joe');
+    expect(set._storage['Joe'] === 2).to.equal(true);
+  });
+
   it('should remove values from a set', function() {
     set.add('Mel Gibson');
     set.remove('Mel Gibson');
     expect(set.contains('Mel Gibson')).to.equal(false);
+  });
+
+  it('should decrement the value if there are more than one item in set', function(){
+    set.add('Bob');
+    set.add('Bob');
+    set.remove('Bob');
+    expect(set._storage['Bob'] === 1).to.equal(true);
   });
 
 });
