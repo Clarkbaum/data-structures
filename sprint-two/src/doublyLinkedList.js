a3c7ea3ee40d97f95b59baa44e94032d8545767a
Hack Reactor Students <accounts@hackreactor.com>
2017-02-03 10:13:37 -0600

    Completed doublyLinkedList.js

--- sprint-two/src/doublyLinkedList.js
+++ sprint-two/src/doublyLinkedList.js
@@ -14,17 +14,33 @@ DoublyLinkedList.prototype.addToTail = function(value) {
     newNode.previous = this.tail;
     this.tail = newNode;
   }
-
 };
 
 DoublyLinkedList.prototype.removeHead = function() {
   if (this.head) {
+    var deleted = this.head;
     this.head = this.head.next;
     this.head.previous = null;
+    return deleted.value;
   } 
 };
 
-DoublyLinkedList.prototype.contains = function() {};
+DoublyLinkedList.prototype.contains = function(target) {
+  var result = false;
+
+  var search = function(node) {
+    if (target === node.value) {
+      result = true;
+    } else {
+      if(node.next) {
+        search(node.next);
+      }
+    }
+  };
+
+  search(this.head);
+  return result;
+};
 
 var DoublyNode = function(value) {
   this.value = value;
